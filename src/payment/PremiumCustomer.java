e3e9a29f4f62c91e5c48cd6321b38d6577425e61
asiye <nouriasiye9@gmail.com>
2025-03-09 13:32:49 -0700

    add premium customer class

---
+++ src/payment/PremiumCustomer.java
@@ -0,0 +1,16 @@
+package payment;
+
+import java.util.ArrayList;
+
+public class PremiumCustomer extends Customer {
+
+    public PremiumCustomer(String name) {
+        super(name);
+        this.paymentHistory = new ArrayList<>();
+    }
+
+    @Override
+    public void displayCustomerInfo() {
+        System.out.println( name + "is" + " " + "Premium Customer");
+    }
+}
